689a77d3989d730ef9a50de9561622b5077ec934
jentzen007 <162001234+jentzen007@users.noreply.github.com>
2024-11-03 16:46:27 +0530

    WORK

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import pages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Paradise-Nursery/',
+  base: './',
 })
